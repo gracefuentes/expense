5ad36b7a12f8fbbc09e97675c73d7642639264c8
sheyla fuentes <sheylag0107@gmail.com>
2020-01-26 20:43:40 -0600

    first real commit

---
+++ expense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  expense
+//
+//  Created by grace on 1/26/20.
+//  Copyright © 2020 grace. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
